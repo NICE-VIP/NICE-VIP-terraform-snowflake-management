2b686751dbd68aea1831ad4bf3d3d6e9648b4c1a
Shreenath Bandivadekar <shreenath@Amits-MacBook-Air.local>
2025-03-18 19:53:55 +0530

    Create 4 users using the USERADMIN role

--- main.tf
+++ main.tf
@@ -7,15 +7,73 @@ terraform {
   }
 }
 
+variable "user_password"{}
+
 provider "snowflake" {
   role = "USERADMIN"
 }
 
-resource "snowflake_user" "user" {
-  name         = "Snowflake_User_01"
-  login_name   = "Snowflake_User_01"
+resource "snowflake_user" "user1" {
+  name         = "Jayesh Soni"
+  login_name   = "jayesh_soni"
 
-  comment      = "User created using the tf-snow user which has the role of USERADMIN"
-  display_name = "Snowflake_User_01"
+  email = "jayyesh26@gmail.com"
+
+  password = var.user_password
+
+  first_name   = "Jayesh"
+  last_name    = "Soni"
+
+  comment      = "User created using the tf-snow user"
+  display_name = "Jayesh Soni"
   must_change_password = "true"
+
+}
+
+resource "snowflake_user" "user2" {
+  name         = "Anuj Joshi"
+  login_name   = "anuj_joshi"
+
+  email = "anujjoshi89@gmail.com"
+
+  password = var.user_password
+
+  first_name   = "Anuj"
+  last_name    = "Joshi"
+
+  comment      = "User created using the tf-snow user"
+  display_name = "Anuj Joshi"
+  must_change_password = "true"
+}
+
+resource "snowflake_user" "user3" {
+  name         = "Samir Wankhede"
+  login_name   = "samir_wankhede"
+
+  email = "samirsw04@gmail.com"
+  password = var.user_password
+
+  first_name   = "Samir"
+  last_name    = "Wankhede"
+
+  comment      = "User created using the tf-snow user"
+  display_name = "Samir Wankhede"
+  must_change_password = "true"
+
+}
+
+resource "snowflake_user" "user4" {
+  name         = "Shreenath Bandivadekar"
+  login_name   = "shreenath_bandivadekar"
+
+  email = "shreenath8.b@gmail.com"
+  password = var.user_password
+
+  first_name   = "Shreenath"
+  last_name    = "Bandivadekar"
+
+  comment      = "User created using the tf-snow user"
+  display_name = "Shreenath Bandivadekar"
+  must_change_password = "true"
+
 }
