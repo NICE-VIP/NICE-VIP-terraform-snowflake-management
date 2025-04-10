76b834fb404746247f47ea6224553d1747e529b7
Samir Wankhede <samirsw04@gmail.com>
2025-04-11 00:18:45 +0530

    infra admin created efficiently

---
+++ variables.tf
@@ -0,0 +1,21 @@
+# variable "alert_email" {
+#   description = "Verified email to receive usage alerts"
+#   type        = string
+# }
+
+# variable "user_password" {
+#   description = "Common password for all users"
+#   type        = string
+#   sensitive   = true
+# }
+
+variable "user_emails" {
+  description = "Map of user emails"
+  type        = map(string)
+  default = {
+    "jayesh_soni"              = "jayyesh26@gmail.com"
+    "anuj_joshi"               = "anujjoshi89@gmail.com"
+    "samir_wankhede"           = "samirsw04@gmail.com"
+    "shreenath_bandivadekar"   = "shreenath8.b@gmail.com"
+  }
+}
