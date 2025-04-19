3563efe1a23d7d278f1deb892e93bc0df1656430
Samir Wankhede <samirsw04@gmail.com>
2025-04-19 16:26:38 +0530

    added infra role

--- main.tf
+++ main.tf
@@ -22,3 +22,12 @@ provider "snowflake" {
   alias  = "security_admin"
   role   = "SECURITYADMIN"
 }
+
+module "roles" {
+  source = "./modules/roles"
+
+  providers = {
+    snowflake.sysadmin       = snowflake.sysadmin
+    snowflake.security_admin = snowflake.security_admin
+  }
+}
