8f19164fba14c763a53b9ee5ad133a7f6419da78
Samir Wankhede <samirsw04@gmail.com>
2025-04-19 19:13:47 +0530

    added monitors creation logic

---
+++ modules/resource_monitors/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "snowflake-labs/snowflake"
+      version = "~> 0.87"
+      configuration_aliases = [ snowflake.accountadmin ]
+    }
+  }
+}
+
+resource "snowflake_resource_monitor" "account_usage_monitor" {
+  name                     = "ACCOUNT_USAGE_MONITOR"
+  provider                 = snowflake.accountadmin
+  credit_quota             = 100
+  notify_triggers          = [80]
+  suspend_trigger          = 90
+  suspend_immediate_trigger = 90
+  notify_users             = ["SAMIRWANKHEDE"]
+}
+
+resource "snowflake_resource_monitor" "warehouse_usage_monitor" {
+  name                     = "WAREHOUSE_USAGE_MONITOR"
+  provider                 = snowflake.accountadmin
+  credit_quota             = 100
+  notify_triggers          = [80]
+  suspend_trigger         = 90
+  suspend_immediate_trigger = 90
+  notify_users             = ["SAMIRWANKHEDE"]
+}
