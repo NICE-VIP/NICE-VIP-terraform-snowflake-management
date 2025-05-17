b201227dc2c29625935750cba139f5073aa91813
Samir Wankhede <samirsw04@gmail.com>
2025-05-17 22:36:22 +0530

    added databases

--- modules/databases/main.tf
+++ modules/databases/main.tf
@@ -1,41 +1,41 @@
-# terraform {
-#   required_providers {
-#     snowflake = {
-#       source  = "snowflake-labs/snowflake"
-#       version = "~> 0.87"
-#       configuration_aliases = [ snowflake.infra_admin, snowflake.sysadmin ]
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "snowflake-labs/snowflake"
+      version = "~> 0.87"
+      configuration_aliases = [ snowflake.infra_admin, snowflake.sysadmin ]
+    }
+  }
+}
 
-# # Locals for reuse
-# locals {
-#   databases = {
-#     APP_DATA_DB = {
-#       comment = "Database for application data."
-#       schema  = "APP_DATA_DB_SCHEMA"
-#     }
-#     LOGS_DB = {
-#       comment = "Database for logging data."
-#       schema  = "LOGS_DB_SCHEMA"
-#     }
-#   }
-# }
+# Locals for reuse
+locals {
+  databases = {
+    APP_DATA_DB = {
+      comment = "Database for application data."
+      schema  = "APP_DATA_DB_SCHEMA"
+    }
+    LOGS_DB = {
+      comment = "Database for logging data."
+      schema  = "LOGS_DB_SCHEMA"
+    }
+  }
+}
 
-# # Create Databases and Schemas
-# resource "snowflake_database" "dbs" {
-#   provider = snowflake.infra_admin
-#   for_each = local.databases
-#   name     = each.key
-#   comment  = each.value.comment
-# }
+# Create Databases and Schemas
+resource "snowflake_database" "dbs" {
+  provider = snowflake.infra_admin
+  for_each = local.databases
+  name     = each.key
+  comment  = each.value.comment
+}
 
-# resource "snowflake_schema" "schemas" {
-#   provider = snowflake.infra_admin
-#   for_each = local.databases
-#   database = each.key
-#   name     = each.value.schema
-# }
+resource "snowflake_schema" "schemas" {
+  provider = snowflake.infra_admin
+  for_each = local.databases
+  database = each.key
+  name     = each.value.schema
+}
 
 # # Grant privileges to data_admin
 # resource "snowflake_grant_privileges_to_account_role" "data_admin_db_privs" {
